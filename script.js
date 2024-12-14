ab9e7690c366fba8f131afc6604b512263cc9f3b
Amzad Ali <amzad.bcastudent20.11477@cimage.in>
2024-12-14 13:12:19 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,65 @@
+const option1 = document.getElementById('option1');
+
+option1.addEventListener('mouseover', () => {
+    const drop1 = document.getElementById('dropdown1');
+    const most = document.getElementById('most-popular1');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
+
+option1.addEventListener('mouseout', () => {
+    const drop1 = document.getElementById('dropdown1');
+    const most = document.getElementById('most-popular1');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
+
+const option2 = document.getElementById('option2');
+
+option2.addEventListener('mouseover', () => {
+    const drop1 = document.getElementById('dropdown2');
+    const most = document.getElementById('most-popular2');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
+
+option2.addEventListener('mouseout', () => {
+    const drop1 = document.getElementById('dropdown2');
+    const most = document.getElementById('most-popular2');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
+
+const option3 = document.getElementById('option3');
+
+option3.addEventListener('mouseover', () => {
+    const drop1 = document.getElementById('dropdown3');
+    const most = document.getElementById('most-popular3');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
+
+option3.addEventListener('mouseout', () => {
+    const drop1 = document.getElementById('dropdown3');
+    const most = document.getElementById('most-popular3');
+
+    drop1.style.display = drop1.style.display === "none" ? "block" : "none";
+
+    // Toggle visibility of most popular section
+    most.style.display = most.style.display === "none" ? "block" : "none";
+})
